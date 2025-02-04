5dd37c94acac2991b89cb3a1479f14b6ebe02191
Aaron Williams <atothea@amazon.com>
2025-02-04 10:20:18 -0600

    testing main

--- main.py
+++ main.py
@@ -3,16 +3,18 @@ from conversion import *
 def convert(initial_unit_name=None, initial_unit_quantity=None, category=None):
     """
     """
-    if type(initial_unit_quantity) not in [int, float] or initial_unit_quantity < 0:
-        raise ValueError("`initial_unit_quantity` must be a non-negative float or int!")
-    
     return UnitConverter(conversion_table, initial_unit_name=initial_unit_name, initial_unit_quantity=initial_unit_quantity, category=category).unit
 
 def print_unit(unit):
     """
     """
+    print(unit)
+    print(f"Starting unit is: {unit.initial_unit_name}")
+    print(f"Starting quanity is: {unit.initial_unit_quantity}")
 
-
+    for units in unit.converted_units:
+        print(f"Unit: {units}, Value:{getattr(unit, units)}")
+    
 
 
 def main():
