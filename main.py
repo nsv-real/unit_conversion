3fde39c3aa6bb6c03859a14f34b1c41820703520
Aaron Williams <atothea@amazon.com>
2025-02-04 10:37:39 -0600

    testing main

--- main.py
+++ main.py
@@ -18,7 +18,22 @@ def print_unit(unit):
 
 
 def main():
-    pass
+    """
+    """
+
+    fluids = [ ["floz", 16, "fluid"], ["liter", 4, "fluid"], ["quart", 4, "fluid"], ["cup", 16, "fluid"] ]
+    weights = [ ["ounce", 16, "weight"], ["pound", 1, "weight"], ["kg", 2.2, "weight" ] ]
+
+    print("Converting fluids...")
+    for fluid in fluids:
+        u = convert(initial_unit_name=fluid[0], initial_unit_quantity=fluid[1], category=fluid[2])
+        print_unit(u)
+
+    print("Converting weights...")
+    for weight in weights:
+        u = convert(initial_unit_name=weight[0], initial_unit_quantity=weight[1], category=weight[2])
+        print_unit(u)
+
 
 if __name__ == "__main__":
     main()
